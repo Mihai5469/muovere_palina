0337014b544691477980151ed73ea940eee9a5c5
Mihai <mihaiiachimovschi2@gmail.com>
2023-04-20 03:12:33 +0200

    ho iniziato a fare un menu per scegliere le carateristiche della palina(non l'ho finito)

--- muovere_palina.cpp
+++ muovere_palina.cpp
@@ -26,6 +26,13 @@ struct palla {
     bool piena;
 };
 
+struct triangolo
+{
+    coordinate punta;
+    coordinate aDestra;
+    coordinate aSinistra;
+};
+
 struct dim_dysplay {
     int w;
     int h;
@@ -33,6 +40,10 @@ struct dim_dysplay {
 
 void init_allegro();        //inizializa la grafica di allegro5
 void install_peri();        //installa le periferiche necesarie
+void draw_menu(coordinate punta);
+void muovi_menu(coordinate& punta, unsigned char key[], palla& p, bool& men);
+
+void draw_triangle(coordinate punta);
 
 void pallina(palla& p);     //disegna la pallina
 void muoviPalla(palla& p, unsigned char key[], dim_dysplay dim);    //muovimento della pallina
@@ -40,12 +51,19 @@ void muoviPalla(palla& p, unsigned char key[], dim_dysplay dim);    //muovimento
 int main()
 {
     bool chiudi = false;
+    bool men = true;
     palla p;
     dim_dysplay d;
+    coordinate punta;
 
+    punta.x = 300;
+    punta.y = 320;
+    
+    //dimensioni finestra
     d.w = 800;
     d.h = 600;
 
+    //pallina
     p.coordinata.x = 400;
     p.coordinata.y = 300;
     p.raggio = 10;
@@ -55,8 +73,6 @@ int main()
     
     init_allegro();     //inizializazione allegro
     install_peri();     //installa le periferiche del computer
-
-    
 
     //variabili allegro
     ALLEGRO_DISPLAY* display = al_create_display(d.w, d.h);
@@ -76,13 +92,15 @@ int main()
     al_start_timer(timer);
     while (true) {
         al_wait_for_event(queue, &event);
-
+        
         switch (event.type)
         {
         case ALLEGRO_EVENT_TIMER:
 
             if (key[ALLEGRO_KEY_ESCAPE])
                 chiudi = true;
+            else if (men)
+                muovi_menu(punta, key, p, men);
             else
                 muoviPalla(p, key, d);
 
@@ -110,7 +128,11 @@ int main()
 
         al_clear_to_color(al_map_rgb(200, 200, 200));
 
-        pallina(p);
+        if(men)
+            draw_menu(punta);
+        else
+            pallina(p);
+        
 
         al_flip_display();
 
@@ -148,6 +170,49 @@ void pallina(palla& p) {
         al_draw_circle(p.coordinata.x, p.coordinata.y, p.raggio, al_map_rgb(r, g, b), 2);
     return;
 }
+void draw_triangle(coordinate punta) {
+    triangolo t;
+    t.punta.x = punta.x;
+    t.punta.y = punta.y + 20;
+    t.aDestra.x = punta.x - 10;
+    t.aDestra.y = punta.y + 40;
+    t.aSinistra.x = punta.x + 10;
+    t.aSinistra.y = punta.y + 40;
+
+    al_draw_filled_triangle(t.punta.x, t.punta.y, t.aDestra.x, t.aDestra.y, t.aSinistra.x, t.aSinistra.y, al_map_rgb(0, 0, 0));
+    return;
+}
+    
+void draw_menu(coordinate punta) {
+    coordinate p1, p2;
+    p1.x = 300;
+    p1.y = 300;
+    p2.x = 500;
+    p2.y = 300;
+
+    al_draw_circle(p1.x, p1.y, 10, al_map_rgb(0, 0, 0), 3);
+    al_draw_filled_circle(p2.x, p2.y, 10, al_map_rgb(0, 0, 0));
+
+    draw_triangle(punta);
+    
+    return;
+}
+void muovi_menu(coordinate& punta, unsigned char key[], palla &p, bool& men) {
+    if (((key[ALLEGRO_KEY_A]) || (key[ALLEGRO_KEY_LEFT])))
+        punta.x = 300;
+    if (((key[ALLEGRO_KEY_D]) || (key[ALLEGRO_KEY_RIGHT])))
+        punta.x = 500;
+    if (key[ALLEGRO_KEY_ENTER]) {
+        if (punta.x == 300)
+            p.piena = false;
+        else
+            p.piena = true;
+        men = false;
+    }
+
+
+    return;
+}
 
 void muoviPalla(palla& p, unsigned char key[], dim_dysplay dim) {
     //muovimento sull'asse x
@@ -160,5 +225,6 @@ void muoviPalla(palla& p, unsigned char key[], dim_dysplay dim) {
         p.coordinata.y--;
     if (((key[ALLEGRO_KEY_S]) || (key[ALLEGRO_KEY_DOWN])) && p.coordinata.y < dim.h - p.raggio)
         p.coordinata.y++;
+
     return;
 }
